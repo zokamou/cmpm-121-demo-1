88e054384f639ef4ccc2a7ac1c8180dfd1fc9f31
zfeller <zfeller@ucsc.edu>
2024-10-09 19:01:01 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,20 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Super duper awesome insane game :D";
 document.title = gameName;
 
+let counter:number = 0;
+
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
 const button = document.createElement("button");
-button.innerHTML = "Click me! &#128516;";
+button.innerHTML = "Click me! 🐢";
+button.addEventListener('click', () => {
+    counter += 1
+    scoreDisplay.innerHTML = counter.toString() + " 🐢";
+})
 app.append(button);
+
+const scoreDisplay = document.createElement("div");
+scoreDisplay.innerHTML = counter.toString() + " 🐢";
+app.append(scoreDisplay);
