1e7c27b7040f2dec9d8ee84b8df27f6285d35d4c
zfeller <zfeller@ucsc.edu>
2024-10-09 18:51:42 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,7 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = "Click me! &#128516;";
+app.append(button);
